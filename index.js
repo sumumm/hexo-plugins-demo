bd6e89c185c4115c23881f8acd0538aa084c3ed6
sumuhk <2038035593@qq.com>
2024-03-26 22:30:15 +0800

    插件获取自身版本号并打印

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 'use strict'; /* 启用strict模式 */
 
 hexo.on('ready', () => {
-    const { version } = require('../../package.json')
-    hexo.log.info(` Welcome to hexo-plugins-demo! ${version} `)
+    const { version } = require('./package.json') // 获取本插件的版本号
+    hexo.log.info(`Welcome to hexo-plugins-demo@${version}`)
 })
